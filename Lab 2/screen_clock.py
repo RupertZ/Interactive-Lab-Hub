a30054eed76857db60afa28bce511d40a3207cd4
RupertZ <zxz397@case.edu>
2021-03-01 23:05:58 -0500

    Master Ma

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,5 @@
 import time
+from time import strftime, sleep
 import subprocess
 import digitalio
 import board
@@ -61,11 +62,63 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+# Enable the buttons for demo
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+
+# Define default coordination
+x, y = 3, 5
+
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
+
+    # Get current time hour 
+    hour = int(strftime("%H"))
+
+    # Wake Up: If Master Ma notices that you get up late, he will criticize you and ask you to practice Kung Fu.
+    # Use bottons to show how it looks like when the current hour is not in that range.
+
+    if (6 <= hour < 8) or (buttonA.value and not buttonB.value): 
+        image = Image.open("mabaoguo3.jpeg")
+        # Resize the image to miniPiTFT resolution 240 * 135
+        image = image.resize((240, 135), Image.BICUBIC)
+
+        draw = ImageDraw.Draw(image)
+
+        draw.text((x, y), strftime("%H:%M:%S%p"), font=font, fill="#005AC8")
+        draw.text((x + 135, y + 90), "Young man,", font=font, fill="#FDAF00")
+        draw.text((x + 135, y + 110), "very rude!", font=font, fill="#FDAF00")
+        
+    # Late Hours： If Master Ma notices that you sleep late， he will kindly urge you to sleep.
+    # Use bottons to show how it looks like when the current hour is not in that range.
+    
+    elif (23 <= hour or hour < 6) or (buttonB.value and not buttonA.value): 
+        image = Image.open("mabaoguo2.jpeg")
+        # Resize the image to miniPiTFT resolution 240 * 135
+        image = image.resize((240, 135), Image.BICUBIC)
+
+        draw = ImageDraw.Draw(image)
+
+        draw.text((x, y), strftime("%H:%M:%S%p"), font=font, fill="#005AC8")
+        draw.text((x + 100, y + 60), "How's weather?", font=font, fill="#FFFFFF")
+        draw.text((x + 100, y + 80), "Think better!", font=font, fill="#FFFFFF")
+
+    # Default: It is business hours and Master Ma shows his master style and manner。
+    else:
+        image = Image.open("mabaoguo1.jpeg")
+        # Resize the image to miniPiTFT resolution 240 * 135
+        image = image.resize((240, 135), Image.BICUBIC)
+
+        draw = ImageDraw.Draw(image)
+        
+        draw.text((x, y), strftime("%H:%M:%S%p"), font=font, fill="#005AC8")
+        draw.text((x, y + 90), "I'm Tai Chi master", font=font, fill="#960000")
+        draw.text((x, y + 110), "Ma Baoguo", font=font, fill="#960000")
+
 
     # Display image.
     disp.image(image, rotation)
